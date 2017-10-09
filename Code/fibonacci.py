634a2ea53792af84d88fd48b9739eddda5bce39b
Michael Laucella <mikel@office.ultratek.com>
2017-10-09 13:40:09 -0400

    added an exeption handled version of fibonnaci with a lookup table

--- Code/fibonacci.py
+++ Code/fibonacci.py
@@ -1,10 +1,38 @@
-def fibonacci(n):
-    if(n <= 1):
-        return n
-    else:
-        return(fibonacci(n-1) + fibonacci(n-2))
-n = int(input("Enter number of terms:"))
-print("Fibonacci sequence:")
-for i in range(n):
-    print(fibonacci(i))
+"""Fibonacci in python with lookup table"""
 
+def fib(x, fibLookup={0:0, 1:1}):
+    """Looks up the nth fibonnaci number
+        uses an enclosed lookuptable to remember
+        values while used and not having global exposure
+        the lookup table can be overwritten if one is provided
+    """
+
+    if x <= 0:
+        return 0
+    elif x == 1:
+        return 1
+
+    try:
+        return fibLookup[x]
+    except:
+        answer = fib(x - 1, fibLookup) + fib(x - 2, fibLookup)
+        fibLookup[x] = answer
+        return answer
+
+if __name__ == "__main__":
+    #enter in value
+    try:
+        x = int(input("Enter terms to display: "))
+    except:
+        print("Must enter in a valid integer")
+        exit(1)
+
+    #create lookup table for displaying all terms rather than one
+    lookup = {0:0,1:1}
+
+    #fibonnaci(x)
+    ans = fib(x, lookup)
+    
+    #display sequence
+    for i in range(x):
+        print(lookup[i])
